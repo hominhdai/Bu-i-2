1c9f923a29c4ef63438001be388affbf59ce2096
hominhdai <125662773+hominhdai@users.noreply.github.com>
2023-04-07 09:58:40 +0700

    Create CTDL&GT_Binary_Recursion_2.cpp

---
+++ CTDL&GT_Binary_Recursion_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+// nguon : GPT
+using namespace std;
+int fib(int n){
+  if(n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+int main() {
+  int kq,n;
+  cout<<"Nhap vi tri n muon tim trong day fibonacci: ";
+  cin>>n;
+  kq = fib(n);
+  cout<<"\nSo fibonacci ? vi tr� "<<n<<" l�: "<<kq;
+}
